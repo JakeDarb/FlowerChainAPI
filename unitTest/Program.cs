f8f10cfc6c7ccc114018a7094588ba15d0e47db6
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2020-11-10 20:04:45 +0100

    added user testing

---
+++ unitTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test!");
+            int addNumber = Add  (4,5);
+            int minusNumber = Minus(10,7);
+
+            Console.WriteLine(addNumber);
+            Console.WriteLine(minusNumber);
+            Console.WriteLine(isOdd(5));
+
+           
+        }
+
+        public static int Add(int x, int y){
+            return x + y;
+        }
+
+        public static int Minus(int a, int b){
+            return a - b;
+        }
+
+        public static Boolean isOdd(int value){
+            return value % 2 == 1;
+        }
+
+        public static Boolean isEven(int value){
+            return value % 2 == 0;
+        }
+    }
+
