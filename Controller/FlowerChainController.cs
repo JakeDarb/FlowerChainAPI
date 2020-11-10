5e09e267fcc5e4865db1cbb68a86f1fbcdcfee28
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2020-11-10 17:26:22 +0100

    endpoints first version

--- Controller/FlowerChainController.cs
+++ Controller/FlowerChainController.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using FlowerChainAPI.Database;
 using FlowerChainAPI.Models.Domain;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace FlowerChainAPI.Controller
@@ -22,9 +23,65 @@ namespace FlowerChainAPI.Controller
          {
              return _context.FlowerBouquet;
          }
-        
-        
+       
 
+        [HttpGet("{id}")]
+        public ActionResult<FlowerBouquet> GetBouquetId(int id)
+        {
+            var bouquetItem = _context.FlowerBouquet.Find(id);
+            
+            if(bouquetItem == null){
+                return NotFound();
+            }else{
+                return bouquetItem;
+            }
+        }
+
+       //Post: FlowerChainAPI/FlowerBouquet
+        [HttpPost]
+        public ActionResult<FlowerBouquet> PostFlowerBouquetItem(FlowerBouquet flowerbouquet)
+        {
+            _context.FlowerBouquet.Add(flowerbouquet);
+            _context.SaveChanges();
+            
+            return CreatedAtAction("GetFlowerBouquetItem", new FlowerBouquet{id= flowerbouquet.id}, flowerbouquet);
+        }
+
+        //Put: FlowerChainAPI/FlowerBouquet/n
+        [HttpPut("{id}")]
+        public ActionResult PutFlowerBouquetItem(int id, FlowerBouquet flowerbouquet)
+        {
+            if(id != flowerbouquet.id){
+                return BadRequest();
+            }
+
+            _context.Entry(flowerbouquet).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+
+
+
+
+
+        //Delete FlowerChainAPI/FlowerBouquet/n
+        [HttpDelete("{id}")]
+        public ActionResult<FlowerBouquet> DeleteFlowerBouquetItem(int id)
+        {
+            var FlowerBouquetItem = _context.FlowerBouquet.Find(id);
+            if(FlowerBouquetItem == null){
+                return NotFound();
+            }else{
+                
+                _context.FlowerBouquet.Remove(FlowerBouquetItem);
+                _context.SaveChanges();
+
+                return FlowerBouquetItem;
+
+            }
+        }
          
      }
  }
